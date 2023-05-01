61f92eaa11ee813b353eb74136c8181f95189c97
nemears <nickmears2@gmail.com>
2023-05-01 02:09:19 -0400

    redirect user to specific project url when they go to main page

--- src/main.js
+++ src/main.js
@@ -6,11 +6,24 @@ import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import ContextMenu from '@imengyu/vue3-context-menu'
 import { UmlWebClient } from 'uml-js';
 
+let sessionName = 'session-' + randomID();
 
-const sessionName = 'session-' + randomID();
-const app = createApp(App);
-app.config.globalProperties.$sessionName = sessionName;
-app.config.globalProperties.$umlWebClient = new UmlWebClient({server: sessionName});
-app.config.unwrapInjectedRef = true;
-app.mount('#app');
-app.use(ContextMenu);
+// this is some logic to determine wether we are accessing an already created project or if we are in a new state.
+if (document.URL.match('.\/projects\/.')) {
+    sessionName = document.URL.match('(?<=\/projects\/).*')[0];
+    const umlWebClient = new UmlWebClient({server: sessionName});
+    const app = createApp(App);
+    app.config.globalProperties.$sessionName = sessionName;
+    app.config.globalProperties.$umlWebClient = umlWebClient;
+    app.config.unwrapInjectedRef = true;
+    app.mount('#app');
+    app.use(ContextMenu);
+} else {
+    // todo create new session
+    const umlWebClient = new UmlWebClient({server: sessionName});
+    const waitForServerAndForwardUrl = async () => {
+        await umlWebClient.reserve();
+        window.location.replace(document.URL + '/projects/' + sessionName);
+    }
+    waitForServerAndForwardUrl();
+}
