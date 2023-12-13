e5683270132f868a8d6e17ad52632ac601257121
nemears <nickmears2@gmail.com>
2023-12-13 11:16:34 -0500

    implemented rule to not allow generalizations to be created between anything but classifiers in class diagrams

--- src/components/diagram/providers/relationships/GeneralizationHandler.js
+++ src/components/diagram/providers/relationships/GeneralizationHandler.js
@@ -7,33 +7,34 @@ export default class GeneralizationHandler extends RuleProvider {
     
     constructor(eventBus, umlWebClient, diagramEmitter, diagramContext, modeling) {
         super(eventBus);
-        RuleProvider.call(this, eventBus);
 
         eventBus.on('connect.end', (event) => {
             if (event.context.start.connectType === 'generalization' || event.connectType === 'generalization') {
-                // create generalization
-                const createGeneralization = async () => {
-                    const generalization = await umlWebClient.post('generalization');
-                    const specific = await umlWebClient.get(event.context.start.modelElement.id);
-                    specific.generalizations.add(generalization);
-                    generalization.general.set(event.hover.modelElement.id);
-                    umlWebClient.put(generalization);
-                    umlWebClient.put(specific);
+                if (event.context.canExecute) {
+                    // create generalization
+                    const createGeneralization = async () => {
+                        const generalization = await umlWebClient.post('generalization');
+                        const specific = await umlWebClient.get(event.context.start.modelElement.id);
+                        specific.generalizations.add(generalization);
+                        generalization.general.set(event.hover.modelElement.id);
+                        umlWebClient.put(generalization);
+                        umlWebClient.put(specific);
 
-                    diagramEmitter.fire('elementUpdate', createElementUpdate(specific));
-                    
-                    event.context.connection = modeling.connect(event.context.start, 
-                        event.hover, 
-                        {
-                            id: randomID(),
-                            modelElement: generalization 
-                        }, {});
+                        diagramEmitter.fire('elementUpdate', createElementUpdate(specific));
+                        
+                        event.context.connection = modeling.connect(event.context.start, 
+                            event.hover, 
+                            {
+                                id: randomID(),
+                                modelElement: generalization 
+                            }, {});
 
-                    // create shape
-                    await createEdge(event.context.connection, umlWebClient, diagramContext);
+                        // create shape
+                        await createEdge(event.context.connection, umlWebClient, diagramContext);
+                    }
+                    createGeneralization();
+                    return false; // stop propogation
                 }
-                createGeneralization();
-                return false; // stop propogation
             }
         });
         eventBus.on('generalization.start', () => {
@@ -43,11 +44,47 @@ export default class GeneralizationHandler extends RuleProvider {
         });
     }
 
+    
+
     init() {
-        this.addRule('connection.start', () => {
-            return true;
+        this.addRule('connection.start', (context) => {
+           return canConnect(context);
+        });
+
+        this.addRule('connection.create', (context) => {
+            return canConnect(context);
+        });
+        this.addRule('connection.reconnect', (context) => {
+            return canConnect(context);
         });
     }
 }
 
 GeneralizationHandler.$inject = ['eventBus', 'umlWebClient', 'diagramEmitter', 'diagramContext', 'modeling'];
+
+function canConnect(context) {
+    const ret = canConnectHelper(context);
+    context.canExecute = ret;
+    return ret;
+}
+
+function canConnectHelper(context) {
+    if (context.source && context.source.connectType === 'generalization') {
+        if (!context.source.modelElement) {
+            return false;
+        }
+        if (!context.source.modelElement.isSubClassOf('classifier')) {
+            return false;
+        }
+        if (context.target) {
+            if (!context.target.modelElement) {
+                return false;
+            }
+            if (!context.target.modelElement.isSubClassOf('classifier')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+} 
