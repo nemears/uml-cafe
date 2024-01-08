d26b15dfffd9241d67e4bedf278fb1aa572ff3e6
nemears <nickmears2@gmail.com>
2024-01-08 13:02:20 -0500

    collapse finished, got rid of UmlEditor and moved functionality to App.vue

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -8,19 +8,16 @@
                 This is an alpha stage open source uml based modeling client. Please be patient with development, or help us with it! There is so much to do! Be aware that the profile and code itself are subject to changes so any projects you create now may not be compatible easily with future versions. To start right click the Model in the element explorer to the left and start creating! 
             </p>
             <p>
-                uml-cafe is built upon a pretty simple technical stack and is made up of 4 parts right now:
+                Check out these open source APIs you can use with this tool!
             </p>
             <p>
-                <a href="https://github.com/nemears/uml-cafe">uml-cafe</a> The vue based application built for the browser that allows interaction with a managed model in the backend. The diagramming ability is built upon bpmn.io's <a href="https://github.com/bpmn-io/diagram-js">diagram-js</a>.
+                <a href="https://github.com/nemears/uml-cafe">uml-cafe</a> The vue based application built for the browser that allows interaction with a managed model in the backend. The diagramming ability is built upon bpmn.io's <a href="https://github.com/bpmn-io/diagram-js">diagram-js</a>. It holds some addition diagramming interfaces that may be useful for development.
             </p>
             <p>
-                <a href="https://github.com/nemears/uml-cpp">uml-cpp</a> The core of the uml data operations in the backend. Keeps a persistent record of uml model elements and allows for aquiring and releasing those elements back to a serialized form in JSON, YAML, or soon XMI.
+                <a href="https://github.com/nemears/uml-cpp">uml-cpp</a> A versatile reconfigurable uml API that can be used in a C++ environment. This also is used to store the projects in the backend of this website!
             </p>
             <p>
-                <a href="https://github.com/nemears/uml-js">uml-js</a> The client api in javascript that allows interaction with an endpont hosting a model or models.
-            </p>
-            <p>
-                <a href="https://github.com/nemears/uml-user-service">uml-user-sercvices</a> The web facing api that allows for sessions per user and shared project states.
+                <a href="https://github.com/nemears/uml-client">uml-client</a> The client API in javascript that allows interaction with a Project hosted using a reusable uml protocol.
             </p>
             <p>
                 Contact: emory@uml.cafe
@@ -32,6 +29,7 @@
 .scrollable {
     overflow-y: auto;
     padding: 10px;
+    flex: 1 1 auto;
 }
 .welcomeText {
     font-size: 75px;
@@ -42,6 +40,7 @@
 }
 .information > p {
     margin: 1rem 0;
+    width: auto;
 }
 a {
   color: rgb(113, 198, 255);
