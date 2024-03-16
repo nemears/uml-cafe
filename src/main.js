f3dd457c8017544c8e423393aaa32a99200421c5
nemears <nickmears2@gmail.com>
2024-03-15 23:27:53 -0400

    finished up project explorer

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import { randomID } from 'uml-client/lib/element';
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import ContextMenu from '@imengyu/vue3-context-menu'
 import { UmlWebClient } from 'uml-client';
+import { getProjectLoginObject } from './umlUtil';
 
 let projectName = randomID();
 let groupName = 'sessions';
-let serverAddress = 'wss://uml.cafe/api/';
+let serverAddress = 'ws://localhost:1672';//'wss://uml.cafe/api/';
 
 // this is some logic to determine wether we are accessing an already created project or if we are in a new state.
 if (location.pathname != "/") {
@@ -16,32 +17,9 @@ if (location.pathname != "/") {
     if (projectUrl.charAt(projectUrl.length - 1) === '/') {
         projectUrl = projectUrl.slice(0, projectUrl.length - 1);
     }
-    const projectUrlSplit = projectUrl.split('/');
-    if (projectUrlSplit.length != 3) {
-        // TODO expand to help user with more info
-        throw new Error("bad url!");
-    }
-    groupName = projectUrlSplit[1];
-    projectName = projectUrlSplit[2];
 
-    // check for stashed user and passwordHash
-    let user = sessionStorage.getItem('user');
-    let passwordHash = sessionStorage.getItem('passwordHash');
-    if (user === 'null' || user === 'undefined') {
-        user = undefined;
-    }
-    if (passwordHash === 'null' || user === 'undefined') {
-        passwordHash = undefined;
-    }
-
-    let umlWebClient = new UmlWebClient({
-        address: serverAddress,
-        group: groupName,
-        project: projectName,
-        user: user,
-        passwordHash: passwordHash,
-        create: groupName !== 'sessions',
-    });
+    let loginObject = getProjectLoginObject(projectUrl, serverAddress); 
+    let umlWebClient = new UmlWebClient(loginObject);
 
     const mountApp = () => {
         const app = createApp(App);
@@ -52,14 +30,14 @@ if (location.pathname != "/") {
     }
 
     umlWebClient.initialization.then(() => {
-        sessionStorage.setItem('user', user);
-        sessionStorage.setItem('passwordHash', passwordHash);
+        sessionStorage.setItem('user', loginObject.user);
+        sessionStorage.setItem('passwordHash', loginObject.passwordHash);
         mountApp();
     }).catch((err) => {
         console.error(err);
         const errObj = JSON.parse(err);
         if (errObj.error.code && errObj.error.code == 1) {
-            if (user) {
+            if (loginObject.user) {
                 // try again with anonymous user
                 umlWebClient = new UmlWebClient({
                     address: serverAddress,
